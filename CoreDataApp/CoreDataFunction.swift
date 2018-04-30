f12eace17fff759c5fae82fb60a1af7c5fc5a919
Chris Brocklesby <git@chrisbrocklesby.com>
2018-04-30 16:58:56 +0100

    Updated different way

--- CoreDataApp/CoreDataFunction.swift
+++ CoreDataApp/CoreDataFunction.swift
@@ -1,110 +1,26 @@
 import UIKit
 import CoreData
 
-///////////////////////// Core Data Variables /////////////////////////
-var entityName = ""
-let appDelegate = UIApplication.shared.delegate as! AppDelegate
-let context = appDelegate.persistentContainer.viewContext
-let entitySet = NSEntityDescription.entity(forEntityName: entityName, in: context)
-let request = NSFetchRequest<NSFetchRequestResult>(entityName: entityName)
+///////////////////////// Section /////////////////////////
 
-
-///////////////////////// Insert Core Data /////////////////////////
-func insertCoreData(entity: String, key: String, value: String) {
-	/// Set Entity of Data
-	entityName = entity
-	
-	/// Insert Data
-	let  insertData = NSManagedObject(entity: entitySet!, insertInto: context)
-	insertData.setValue(value, forKey: key)
-	print("Core Data: Inserted")
-}
-
-
-///////////////////////// Save Core Data /////////////////////////
-func saveCoreData(entity: String) {
-	/// Set Entity of Data
-	entityName = entity
-	
-	/// Save Data
-	do {
-		try context.save()
-		print("Core Data: Saved")
-	} catch {
-		print("Core Data: Save - \(error)")
-		return
+func insertCoreData(){
+	if let coreData = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+		let toDo = ToDoCoreData(entity: ToDoCoreData.entity(), insertInto: coreData)
+		toDo.name = "Goto Mary"
+		toDo.important = true
+		try? coreData.save()
 	}
+	
 }
 
+///////////////////////// Section /////////////////////////
 
-///////////////////////// Index Core Data /////////////////////////
-func indexCoreData(entity: String) {
-	/// Set Entity of Data
-	entityName = entity
-	
-	/// Request Settings
-	request.returnsObjectsAsFaults = false // Performance Gain
-
-	/// Get All Data for Index
-	do {
-		// Fetch
-		let result = try context.fetch(request)
-		// Loop
-		for object in result as! [NSManagedObject] {
-			print(object.value(forKey: "name") as! String)
-			print(object.value(forKey: "country") as! String)
-		}
-		print("Core Data: Get Index Success")
-	} catch {
-		print("Core Data: Index - \(error)")
-	}
-}
-
-
-///////////////////////// Get Core Data /////////////////////////
-func getCoreData(entity: String, key: String, value: String) {
-	/// Set Entity of Data
-	entityName = entity
-	
-	/// Request Settings and Criteria
-	request.predicate = NSPredicate(format: "\(key) = '\(value)'")
-	request.returnsObjectsAsFaults = false // Performance Gain
-	
-	/// Get Data based on Request
-	do {
-		// Fetch
-		let result = try context.fetch(request)
-		// Loop
-		for object in result as! [NSManagedObject] {
-			print(object.value(forKey: "name") as! String)
-			print(object.value(forKey: "country") as! String)
-		}
-		print("Core Data: Get Successful")
-	} catch {
-		print("Core Data: Get - \(error)")
-	}
-}
-
-
-///////////////////////// Delete Core Data /////////////////////////
-func deleteCoreData(entity: String, key: String, value: String) {
-	/// Set Entity of Data
-	entityName = entity
-	
-	/// Request Settings and Criteria
-	request.predicate = NSPredicate(format: "\(key) = '\(value)'")
-	request.returnsObjectsAsFaults = false // Cache
-	
-	/// Delete Data based on Request
-	do {
-		// Fetch
-		let result = try context.fetch(request)
-		// Loop
-		for object in result {
-			context.delete(object as! NSManagedObject)
-		}
-		print("Core Data: Deleted")
-	} catch {
-		print("Core Data: Delete - \(error)")
+func getCoreData(){
+	if let coreData = (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer.viewContext {
+		let coreDataToDos = try? coreData.fetch(ToDoCoreData.fetchRequest()) as? [ToDoCoreData]
+			if let theToDos = coreDataToDos {
+				print(theToDos?.last?.name)
+			}
+		
 	}
 }
--- CoreDataApp/ViewController.swift
+++ CoreDataApp/ViewController.swift
@@ -5,15 +5,8 @@ class ViewController: UIViewController {
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
-		//insertCoreData(entity: "Users", key: "name", value: "Bob")
-		
-		//saveCoreData(entity: "Users")
-		
-		//deleteCoreData(entity: "Users", key: "name", value: "Tommy")
-		
-		indexCoreData(entity: "Users")
-		
-		//getCoreData(entity: "Users", key: "name", value: "Bob")
+		insertCoreData()
+		getCoreData()
 	}
 
 }
